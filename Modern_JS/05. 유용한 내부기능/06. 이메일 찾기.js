9ed47960e19d608216cdee2be2dd191b64b499c9
Yoojungchoii <geniusyj@gmail.com>
2023-12-20 00:57:10 +0900

    fine method example added

---
+++ Modern_JS/05. 유용한 내부기능/06. 이메일 찾기.js
@@ -0,0 +1,41 @@
+//find 사용하는 예제
+// find : 요소 찾기. 하나의 값만 리턴하고 종료됨.
+
+const nameInput = document.querySelector('#user-name');
+const phoneInput = document.querySelector('#phone-number');
+const findBtn = document.querySelector('#find');
+
+const data = [
+  { userName: '막대기', phoneNumber: '01012341111', email: 'stick@go_do_it.kr' },
+  { userName: 'young', phoneNumber: '01012342222', email: 'kang@go_do_it.kr' },
+  { userName: '코린이', phoneNumber: '01012343333', email: 'corin2@go_do_it.kr' },
+  { userName: 'captain', phoneNumber: '01012344444', email: 'crew@go_do_it.kr' },
+  { userName: 'YH', phoneNumber: '01012345555', email: 'whyH@go_do_it.kr' },
+  { userName: '망고쥬스', phoneNumber: '01012346666', email: 'drinkMango@go_do_it.kr' },
+  { userName: 'nemoming', phoneNumber: '01012347777', email: 'ractAngle@go_do_it.kr' },
+  { userName: '강그루', phoneNumber: '01012348888', email: 'riverTree@go_do_it.kr' },
+  { userName: '개룩발룩', phoneNumber: '01012349999', email: 'checkShirts@go_do_it.kr' },
+  { userName: '오렌지쥬스', phoneNumber: '01012341010', email: 'delmonte@go_do_it.kr' },
+];
+
+function findEmail() {
+  const nameValue = nameInput.value;
+  const phoneValue = phoneInput.value;
+
+  // 여기에 코드를 작성하세요
+
+  // 원래함수.find(function(현재의 요소) { 실행할 문장} )
+  // Arrow 함수로 바꾸면 function과 중괄호를 생략 가능하다. 
+  const user = data.find((el) => 
+  // 두 값을 찾을 때 두 조건 모두를 만족시키려면 &&를 꼭 써야한다. 
+    nameValue === el.userName &&  phoneValue === el.phoneNumber);
+
+
+  const message = user
+    ? `${user.userName}님의 이메일은 ${user.email} 입니다.`
+    : '이메일을 찾을 수 없습니다. 입력 정보를 다시 확인해 주세요.';
+
+  alert(message);
+}
+
+findBtn.addEventListener('click', findEmail);
