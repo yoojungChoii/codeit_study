bd29d6d128b525e3ab11e3f6328033aec0159bd1
Yoojungchoii <geniusyj@gmail.com>
2024-01-01 23:25:25 +0900

    HandIcon-2 added

--- React/가위바위보 게임/rock-scissor-paper/src/HandIcon.js
+++ React/가위바위보 게임/rock-scissor-paper/src/HandIcon.js
@@ -1,10 +1,19 @@
-import rock from './assets/rock.svg'
+import rockImg from './assets/rock.svg';
+import scissorImg from './assets/scissor.svg';
+import paperImg from './assets/paper.svg';
 
-// 함수의 첫 글자는 Rock
-function HandIcon() {
+const IMAGES = {
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+};
+
+// 함수의 첫 글자는 대문자
 // img 같이 내용 없는 태그는 /로 self-closing 해야함
 // return JSX태그
-  return <img src = {rock} alt = "rock" />;
+function HandIcon({ value }) {
+  const src = IMAGES[value];
+  return <img src={src} alt={value} />;
 }
 
 export default HandIcon;
