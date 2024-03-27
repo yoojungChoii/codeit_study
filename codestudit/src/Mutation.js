1f8598c94b1f5ff5f2e44bef55d7ff41cb4deaf3
Yoojungchoii <geniusyj@gmail.com>
2024-03-27 15:37:03 +0900

    useMutate 예시 함수와 주석

---
+++ codestudit/src/Mutation.js
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+// 데이터 업데이트 함수
+const updateUser = (userId, updateData) => {
+  return fetch(`/api/user/${userId}`, {
+    method: "PUT",
+    body: JSON.stringify(updateData),
+  }).then((response) => response.json());
+};
+
+UserProfileEditor = ({ userId }) => {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation(
+    (updatedData) => updateUser(userId, updatedData),
+    {
+      onSuccess: () => {
+        // 데이터 업데이트 후 캐시를 재로드
+        queryClient.invalidateQueries(["user", userId]);
+      },
+    }
+  );
+
+  const handleSubmit = (updatedData) => {
+    // mutate는 자동 실행이 되지 않기 때문에 submit 시 mutate 실행시켜줘야함
+    mutate(updatedData);
+  };
+
+  return (
+    <div>
+      <h2>Edit User Profile</h2>
+      <UserForm onSubmit={handleSubmit} />
+    </div>
+  );
+};
